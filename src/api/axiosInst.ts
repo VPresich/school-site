e69073fcb43050145928ff0753401f8168e2f809
VPresich <valya.alexandrova.presich@gmail.com>
2025-12-28 20:27:38 +0100

    Add testing info

--- src/api/axiosInst.ts
+++ src/api/axiosInst.ts
@@ -2,14 +2,14 @@ import axios from 'axios';
 
 const projectId = import.meta.env.VITE_PROJECTID;
 const dataset = import.meta.env.VITE_DATASET;
-const tocken = import.meta.env.VITE_TOKEN_READ;
+const token = import.meta.env.VITE_TOKEN_READ;
 console.log('projectId', projectId);
 console.log('dataset', dataset);
-console.log('tocken', tocken);
+console.log('token', token);
 
 export const axiosInst = axios.create({
   baseURL: `https://${projectId}.api.sanity.io/v2023-08-01/data/query/${dataset}`,
   headers: {
-    Authorization: `Bearer ${import.meta.env.VITE_TOKEN_READ}`,
+    Authorization: `Bearer ${token}`,
   },
 });
